d10051d58277421cf0f2442bc68c01f00531111f
Eghosa Ordia <eghordia130@gmail.com>
2023-09-09 10:06:31 +0100

    refactor

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+let e = !1;
+const s = document.querySelector("#navlinks"),
+  l = document.querySelector("#hamburger"),
+  t = document.querySelector("#navLayer"),
+  i = [...s.querySelector("ul").children];
+function a() {
+  e
+    ? (s.classList.add(
+        "!visible",
+        "!scale-100",
+        "!opacity-100",
+        "!lg:translate-y-0"
+      ),
+      l.classList.add("toggled"),
+      t.classList.add("origin-top", "scale-y-100"))
+    : (s.classList.remove(
+        "!visible",
+        "!scale-100",
+        "!opacity-100",
+        "!lg:translate-y-0"
+      ),
+      l.classList.remove("toggled"),
+      t.classList.remove("origin-top", "scale-y-100"));
+}
+l.addEventListener("click", () => {
+  (e = !e), a();
+});
+i.forEach((c) => {
+  c.addEventListener("click", () => {
+    (e = !e), a();
+  });
+});
+
+// Text animation carousel
 let texts = document.querySelectorAll(".texts");
 let currentIndex = 0;
 
@@ -19,17 +53,5 @@ function toggleText() {
 texts.forEach((text, index) => {
   if (index !== currentIndex) {
     text.classList.add("hidden");
-  }
-});
-
-// Set an interval to toggle between text elements infinitely
-setInterval(toggleText, 3000); // Adjust the timing as needed
-
-let hamburger = document.getElementById("hamburger");
-// let navlinks = document.getElementById("navlinks");
-// let navLayer = document.getElementById("navLayer");
-hamburger.addEventListener("click", function () {
-  {
-    document.body.classList.toggle("clicked");
   }
 });
